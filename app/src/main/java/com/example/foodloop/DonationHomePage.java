6ee16ab6e7a76792f53f81bec226f106a2a70de6
Belle Mercado <bellemercado@Belles-MacBook-Pro.local>
2026-03-28 14:17:58 -0700

    added Donation Home Page java and layout, and donor page java and layout

---
+++ app/src/main/java/com/example/foodloop/DonationHomePage.java
@@ -0,0 +1,32 @@
+package com.example.foodloop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DonationHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_donation_home_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Donate Food button
+        Button btnDonateFood = findViewById(R.id.btnDonate);
+        btnDonateFood.setOnClickListener(view->{
+            startActivity(new Intent(DonationHomePage.this, DonorPage.class));
+        });
+    }
+}
