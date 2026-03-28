2308c4e2acdd5d28d321e5c6d143de3950559720
gkcsupetran <gkcsupetran@gmail.com>
2026-03-28 15:31:23 -0700

    Request Tracker added -G

--- app/src/main/java/com/example/foodloop/ActiveRequests.java
+++ app/src/main/java/com/example/foodloop/ActiveRequests.java
@@ -10,24 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActiveDonations extends AppCompatActivity {
-
+public class ActiveRequests extends AppCompatActivity {
     Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_active_donations);
+        setContentView(R.layout.activity_active_requests);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnHome = findViewById(R.id.btnMADtoHome);
+        btnHome = findViewById(R.id.btnRTtoHome);
         btnHome.setOnClickListener(v -> {
-            startActivity(new Intent(ActiveDonations.this, MainActivity.class));
+            startActivity(new Intent(ActiveRequests.this, MainActivity.class));
         });
     }
 }
