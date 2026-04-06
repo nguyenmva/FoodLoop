c878eef02c8f482869839fef30833213b3144a40
gkcsupetran <gkcsupetran@gmail.com>
2026-04-06 16:22:39 -0700

    Added the delete function to the Edit Donation page -G

--- app/src/main/java/com/example/foodloop/DatabaseHelper.java
+++ app/src/main/java/com/example/foodloop/DatabaseHelper.java
@@ -219,6 +219,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         int result = db.update(USERS_TABLE, contentValues, USER_EMAIL_FLD + " = ?", new String[]{email});
         return result != -1;
     }
+
     // ##################################################################################################################
     // FOR EDITING A DONATION THAT IS ALREADY LISTED
     public boolean editDonation(int donationID, String itemName, int quantity, String category, int categorySpinner,
@@ -239,6 +240,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         return result > 0; //This will return true if at least 1 row was edited
     }
+
+    // ##################################################################################################################
+    // Delete an Existing Donation Listing - Gia
+    public boolean deleteDonation(int donationID) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        int result = db.delete(DONATION_TABLE, DONATION_ID_FLD + " = ?", new String[]{String.valueOf(donationID)});
+        return result > 0;
+    }
+
 
     // ##################################################################################################################
     // FOR UPDATING A DONATION'S STATUS
